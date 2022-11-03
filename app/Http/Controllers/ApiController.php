34acf7eac8e76c81553063ea991d6deec5f5f80d
arun-web-dev <miuiandrew@gmail.com>
2022-11-03 17:46:34 +0530

    createarea and createzone updated

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Area;
+use App\Models\Zone;
+use Exception;
 use Illuminate\Auth\Events\Validated;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -27,38 +30,190 @@ class ApiController extends Controller
         $role = $request->role;
         $password = $request->password;
 
-        $invID =0;
-         $maxValue = DB::table('users')->max('id');
-         $invID=$maxValue+1;
-         $invID = str_pad($invID, 3, '0', STR_PAD_LEFT);
-         $userid="BB-USR-".$invID;
+        $invID = 0;
+        $maxValue = DB::table('users')->max('id');
+        $invID = $maxValue + 1;
+        $invID = str_pad($invID, 3, '0', STR_PAD_LEFT);
+        $userid = "BB-USR-" . $invID;
 
-         $saveuser = DB::table('users')->insert([
-            "name"=>$name,
-            "email"=>$email,
-            "mobilenumber"=>$mobilenumber,
-            "role"=>$role,
-            "password"=>$password,
-            "userid"=>$userid
-         ]);
+        $saveuser = DB::table('users')->insert([
+            "name" => $name,
+            "email" => $email,
+            "mobilenumber" => $mobilenumber,
+            "role" => $role,
+            "password" => $password,
+            "userid" => $userid
+        ]);
 
-         if($saveuser)
-         {
+        if ($saveuser) {
             // return response()->json([
             //     // 'status' => '200',
             //     'message' => 'Register Successfully',
             // ]);
 
-            return ["Result"=>"User Created"];
-         }
-         else
-         {
+            return ["Result" => "User Created"];
+        } else {
             // return response()->json([
             //     // 'status' => '200',
             //     'message' => 'Register Failed',
             // ]);
-         }
+        }
+    }
 
 
+
+
+    public function createzone(Request $request)
+    {
+        try {
+            $validated = $request->validate([
+                'zoneid' => 'required',
+                'mainzone' => 'required',
+                'subzone' => 'required',
+            ]);
+
+            $data =   Zone::create($request->all());
+
+            return ["Result" => "Zone Created", "data" => $data];
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+    }
+
+
+    // Create Area
+    public function createArea(Request $request)
+    {
+        try {
+            $request->validate([
+                'areaname' => 'required',
+            ]);
+
+
+            $data = Area::create($request->all());
+
+            return ["Result" => "Area Created", "data" => $data];
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+    }
+
+
+    // UpdateArea
+    public function updateArea(Request $request, $id)
+    {
+
+        try {
+            $request->validate([
+                'areaname' => 'required',
+            ]);
+            $data = Area::findOrFail($id);
+            $data->update($request->all());
+
+            return ["Result" => "Area Updated", "data" => $data];
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+    }
+
+    // View Area
+    public function viewArea($id)
+    {
+        try {
+            $data = Area::findOrFail($id);
+            return ["Result" => "Area", "data" => $data];
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+    }
+
+
+
+    // To delete Area
+    public function deleteArea($id)
+    {
+        try {
+            $data = Area::findOrFail($id);
+            $data->delete();
+
+            return ["Result" => "Area Deleted", "data" => $data];
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+    }
+
+
+
+
+    // Create Zone
+    public function createzone(Request $request)
+    {
+        try {
+            $request->validate([
+                'zoneid' => 'required',
+            ]);
+
+
+            $data = Zone::create($request->all());
+
+            return ["Result" => "Zone Created", "data" => $data];
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+    }
+
+    // View Zone
+    public function viewzone($id)
+    {
+        try {
+            $data = Zone::findOrFail($id);
+            return ["Result" => "Zone Data", "data" => $data];
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+    }
+
+
+
+    // UpdateZone
+    public function updatezone(Request $request, $id)
+    {
+
+        try {
+            $request->validate([
+                'zoneid' => 'required',
+            ]);
+            $data = Zone::findOrFail($id);
+            $data->update($request->all());
+
+            return ["Result" => "Zone Updated", "data" => $data];
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+    }
+
+    // View Area
+    public function viewzone($id)
+    {
+        try {
+            $data = Zone::findOrFail($id);
+            return ["Result" => "Zone Data", "data" => $data];
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+    }
+
+
+
+    // To delete Zone
+    public function deletezone($id)
+    {
+        try {
+            $data = Zone::findOrFail($id);
+            $data->delete();
+            return ["Result" => "Zone Deleted", "data" => $data];
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,29 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::POST('createuser',[ApiController::class,'createuser']);
+Route::POST('createuser', [ApiController::class, 'createuser']);
+
+// To create Area
+Route::POST('createarea', [ApiController::class, "createArea"]);
+
+// To view Area
+Route::get('area/{id}', [ApiController::class, "viewArea"]);
+
+// To Update Area
+Route::POST('updatearea/{id}', [ApiController::class, "updateArea"]);
+
+// To Delete Area
+Route::post('deletearea/{id}', [ApiController::class, "deleteArea"]);
+
+
+// To create Zone
+Route::POST('createzone', [ApiController::class, "createzone"]);
+
+// To view zone
+Route::get('zone/{id}', [ApiController::class, "viewzone"]);
+
+// To Update zone
+Route::POST('updatezone/{id}', [ApiController::class, "updatezone"]);
+
+// To Delete zone
+Route::post('deletezone/{id}', [ApiController::class, "deletezone"]);
