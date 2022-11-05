a3dbb5eb79268bbc214bab00ebef8c1a73b7be79
KesavarajKr <kesavaraj.kr@gmail.com>
2022-11-05 10:20:58 +0530

    split layouts

---
+++ resources/views/layout/navbar.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/slick.css">
+    <script src="https://kit.fontawesome.com/cdcced96ff.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+
+    <header>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-2 text-center">
+                    <div class="logo">
+                        <img src="assets/images/dashboard/business_bench_logo.svg" alt="">
+                    </div>
+                </div>
+                <div class="col-lg-8">
+                    <div class="navbar">
+                        <ul>
+                            <li><a href="javascript:;">Home</a></li>
+                            <li><a href="javascript:;">Projects</a></li>
+                            <li><a href="javascript:;">Zones</a></li>
+                            <li><a href="javascript:;">Region</a></li>
+                            <li><a href="javascript:;">Area</a></li>
+                            <li><a href="javascript:;">Engineers</a></li>
+                            <li><a href="javascript:;">Users</a></li>
+                            <li><a href="javascript:;">Clients</a></li>
+                            <li><a href="javascript:;">Estimate</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-2">
+                    <div class="admin_logo">
+                        <img src="" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
